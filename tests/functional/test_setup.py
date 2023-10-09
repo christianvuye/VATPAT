12afcc348c534b1f7ffcb7329e4a479a136c4a79
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2023-10-09 19:58:15 +0200

    Setup Django project with test folders and pytest for TDD, configured python-decoupler for separating config from code

---
+++ tests/functional/test_setup.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+def test_initial_page_title():
+    browser = webdriver.Firefox()
+    browser.get("http://localhost:8000")
+    assert "Congratulations" in browser.title
+    browser.quit()  # Don't forget to close the browser to clean up
---
+++ tests/unit/test_secret_key.py
@@ -0,0 +1,6 @@
+import pytest
+from django.conf import settings
+
+def test_secret_key():
+    assert hasattr(settings, 'SECRET_KEY'), "SECRET_KEY setting not found"
+    assert settings.SECRET_KEY != '', "SECRET_KEY is empty"
