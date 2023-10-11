09f06f500fb551843335b45b796d973e3dcdb490
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2023-10-11 09:45:56 +0200

    Refactor Dealers model to pass unit tests and rename unit tests in dashboard app models to be more easily recognizable

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 class Dealers(models.Model):
-    name = models.CharField(max_length=255)
+    D_ID = models.CharField(max_length=10, unique=True)
+    DealerName = models.CharField(max_length=100)
+    DealerVATnumber = models.CharField(max_length=20)
+    DealerEmail = models.CharField(max_length=80)
+    CreatedDate = models.DateTimeField()
+    ModifiedDate = models.DateTimeField()
+
