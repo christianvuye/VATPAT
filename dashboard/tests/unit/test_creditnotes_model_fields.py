aa9e2a9b892cdaea21d0e1681ee854d901d9def3
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2023-10-11 10:07:43 +0200

    Add unit test to check for the correct fields in the credit notes model in dashboard app

---
+++ dashboard/tests/unit/test_creditnotes_model_fields.py
@@ -0,0 +1,45 @@
+from django.apps import apps
+
+def test_creditnotes_model_fields():
+    try:
+        CreditNotes = apps.get_model('dashboard', 'creditnotes')
+    except LookupError:
+        assert False, "The 'creditnotes' model does not exist."
+    
+    # Test CN_ID field
+    cn_id_field = CreditNotes._meta.get_field('CN_ID')
+    assert cn_id_field.max_length == 20, "CN_ID field max_length does not match"
+    assert cn_id_field.unique == True, "CN_ID field is not unique"
+    assert cn_id_field.get_internal_type() == 'CharField', "CN_ID field type is not CharField"
+
+    # Test D_ID field
+    d_id_field = CreditNotes._meta.get_field('D_ID')
+    assert d_id_field.max_length == 10, "D_ID field max_length does not match"
+    assert d_id_field.get_internal_type() == 'CharField', "D_ID field type is not CharField"
+
+    # Test TotalDocumentAmount field
+    total_document_amount_field = CreditNotes._meta.get_field('TotalDocumentAmount')
+    assert total_document_amount_field.get_internal_type() == 'DecimalField', "TotalDocumentAmount field type is not DecimalField"
+    assert total_document_amount_field.max_digits == 38, "TotalDocumentAmount field max_digits does not match"
+    assert total_document_amount_field.decimal_places == 20, "TotalDocumentAmount field decimal_places does not match"
+
+    # Test TotalVATAmountDocumentt field
+    total_vat_amount_document_field = CreditNotes._meta.get_field('TotalVATAmountDocumentt')
+    assert total_vat_amount_document_field.get_internal_type() == 'DecimalField', "TotalVATAmountDocumentt field type is not DecimalField"
+    assert total_vat_amount_document_field.max_digits == 38, "TotalVATAmountDocumentt field max_digits does not match"
+    assert total_vat_amount_document_field.decimal_places == 20, "TotalVATAmountDocumentt field decimal_places does not match"
+
+    # Test TotalDocumentAmountWithVAT field
+    total_document_amount_with_vat_field = CreditNotes._meta.get_field('TotalDocumentAmountWithVAT')
+    assert total_document_amount_with_vat_field.get_internal_type() == 'DecimalField', "TotalDocumentAmountWithVAT field type is not DecimalField"
+    assert total_document_amount_with_vat_field.max_digits == 38, "TotalDocumentAmountWithVAT field max_digits does not match"
+    assert total_document_amount_with_vat_field.decimal_places == 20, "TotalDocumentAmountWithVAT field decimal_places does not match"
+
+    # Test AccountingNumberID field
+    accounting_number_id_field = CreditNotes._meta.get_field('AccountingNumberID')
+    assert accounting_number_id_field.max_length == 30, "AccountingNumberID field max_length does not match"
+    assert accounting_number_id_field.get_internal_type() == 'CharField', "AccountingNumberID field type is not CharField"
+
+    # Test IssuedDate field
+    issued_date_field = CreditNotes._meta.get_field('IssuedDate')
+    assert issued_date_field.get_internal_type() == 'DateTimeField', "IssuedDate field type is not DateTimeField"
