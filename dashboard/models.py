dd4c9fb7e56d8d0554e03e41ba2c6a09401331f0
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2023-10-11 10:10:26 +0200

    Update dashboard app models to pass credit note model fields unit test

--- dashboard/models.py
+++ dashboard/models.py
@@ -9,4 +9,10 @@ class Dealers(models.Model):
     ModifiedDate = models.DateTimeField()
 
 class CreditNotes(models.Model):
-    pass
+    CN_ID = models.CharField(max_length=20, unique=True)
+    D_ID = models.CharField(max_length=10)
+    TotalDocumentAmount = models.DecimalField(max_digits=38, decimal_places=20)
+    TotalVATAmountDocumentt = models.DecimalField(max_digits=38, decimal_places=20)
+    TotalDocumentAmountWithVAT = models.DecimalField(max_digits=38, decimal_places=20)
+    AccountingNumberID = models.CharField(max_length=30)
+    IssuedDate = models.DateTimeField()
