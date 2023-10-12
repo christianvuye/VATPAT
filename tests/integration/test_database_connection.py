143f5f559e31996dcebea877613bdc991b546b3b
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2023-10-12 12:20:36 +0200

    Modify database connection integration test to reduce scope of test to pyodbc only.

--- tests/integration/test_database_connection.py
+++ tests/integration/test_database_connection.py
@@ -1,13 +1,22 @@
-from django.db import connections
-from django.db.utils import OperationalError
+import pyodbc
+from django.conf import settings
 
 def test_db_connection():
-    db_conn = connections['default']
+    db_settings = settings.DATABASES['default']
+    
+    connection_string = (
+        f"DRIVER={{{db_settings['OPTIONS']['driver']}}};" 
+        f"SERVER={db_settings['HOST']};"
+        f"DATABASE={db_settings['NAME']};"
+        f"UID={db_settings['USER']};"
+        f"PWD={db_settings['PASSWORD']};"
+        f"{db_settings['OPTIONS']['Extra Params']}"
+    )
+
+    print(f"Connection String: {connection_string}")
+
     try:
-        db_conn.connect()
-    except OperationalError:
-        assert False, "Database connection failed!"
-    else:
-        assert True
-    finally:
-        db_conn.close()
+        with pyodbc.connect(connection_string):
+            pass
+    except pyodbc.Error as e:
+        assert False, f"Database connection failed! Error: {e}"
