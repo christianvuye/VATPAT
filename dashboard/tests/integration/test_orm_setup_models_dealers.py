42e548b3760f8b45ebdbb06f087ef46dbf93fc91
christianvuye <christian.vuye.ext@honda-eu.com>
2023-11-28 16:23:27 +0100

    update test function with dummy data for test database

--- dashboard/tests/integration/test_orm_setup_models_dealers.py
+++ dashboard/tests/integration/test_orm_setup_models_dealers.py
@@ -1,50 +1,20 @@
 import pytest
+from django.utils import timezone
 from dashboard.models import Dealers
 
-# The @pytest.mark.django_db decorator enables this test to access the Django database.
-# It wraps the test in a transaction (rolled back after test) and resets auto-increment sequences,
-# ensuring test isolation and consistent primary key sequences for each test run.
-
-'''
-Here is a verbose explanation of what the @pytest.mark.django_db decorator does:
-    python
-    import pytest
-
-    def django_db_decorator(test_func):
-        def wrapper(*args, **kwargs):
-            # Start a database transaction
-            db.start_transaction()
-        
-            # Reset database sequences to initial values
-            db.reset_sequences()
-        
-            # Run the test function
-            test_result = test_func(*args, **kwargs)
-        
-            # Rollback the database transaction to undo any changes
-            db.rollback_transaction()
-        
-            return test_result
-        return wrapper
-
-    @django_db_decorator
-        def my_test():
-            # Interact with the database
-            MyModel.objects.create(...)
-    
-    my_test()
-
-In words, the @pytest.mark.django_db decorator:
-1. Starts a database transaction before running each test
-2. Resets the database sequences to their initial values (to prevent primary key collisions between tests)
-3. Runs the actual test function
-4. Rolls back the transaction after the test finishes, undoing any changes made to the database by the test
-5. This allows each test to operate within its own isolated database transaction, 
-   avoid interfering with other tests, and start from a clean initial database state. 
-   The resetting of sequences also prevents primary key conflicts between tests.
-'''
 @pytest.mark.django_db(transaction=True, reset_sequences=True)
-def test_orm_setup_for_dealers():
+def test_orm_setup_for_dealers():   
+    now = timezone.now()  # This will be a timezone-aware datetime object
+    
+    Dealers.objects.create(
+        D_ID='D001',
+        DealerName='Test Dealer',
+        DealerVATnumber='123456789',
+        DealerEmail='test@example.com',
+        CreatedDate=now,
+        ModifiedDate=now
+    )
+
     all_dealers = Dealers.objects.all()
 
     # Ensure we can fetch dealers, indicating no basic mismatch
