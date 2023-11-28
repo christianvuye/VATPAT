d85fbeed7a29dbc4e097c8706b2bc24f7fac3fbd
christianvuye <christian.vuye.ext@honda-eu.com>
2023-11-28 15:08:03 +0100

    add comment to test decorator

--- dashboard/tests/integration/test_orm_setup_models_dealers.py
+++ dashboard/tests/integration/test_orm_setup_models_dealers.py
@@ -1,6 +1,10 @@
 import pytest
 from dashboard.models import Dealers
 
+# The @pytest.mark.django_db decorator enables this test to access the Django database.
+# It wraps the test in a transaction (rolled back after test) and resets auto-increment sequences,
+# ensuring test isolation and consistent primary key sequences for each test run.
+
 @pytest.mark.django_db(transaction=True, reset_sequences=True)
 def test_orm_setup_for_dealers():
     all_dealers = Dealers.objects.all()
