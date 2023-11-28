2b931107d556730479967e50ba65004abbfbd550
christianvuye <christian.vuye.ext@honda-eu.com>
2023-11-28 16:34:42 +0100

    Add create_dealer fixture and improve test assertions

--- dashboard/tests/integration/test_orm_setup_models_dealers.py
+++ dashboard/tests/integration/test_orm_setup_models_dealers.py
@@ -2,11 +2,10 @@ import pytest
 from django.utils import timezone
 from dashboard.models import Dealers
 
-@pytest.mark.django_db(transaction=True, reset_sequences=True)
-def test_orm_setup_for_dealers():   
-    now = timezone.now()  # This will be a timezone-aware datetime object
-    
-    Dealers.objects.create(
+@pytest.fixture
+def create_dealer():
+    now = timezone.now()  # This will be a timezone-aware datetime
+    return Dealers.objects.create(
         D_ID='D001',
         DealerName='Test Dealer',
         DealerVATnumber='123456789',
@@ -15,7 +14,10 @@ def test_orm_setup_for_dealers():
         ModifiedDate=now
     )
 
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
+def test_orm_setup_for_dealers(create_dealer):   
     all_dealers = Dealers.objects.all()
 
     # Ensure we can fetch dealers, indicating no basic mismatch
     assert all_dealers.exists(), "Failed to fetch dealers from the database."
+    assert create_dealer in all_dealers, "Created dealer is not in the database."
