bea893cf5e443655835447384dca1ccdca9eb3cf
christianvuye <christian.vuye.ext@honda-eu.com>
2023-11-29 16:34:50 +0100

    update credit note model to reference dealer model as foreign key and pass integratioin test

--- dashboard/models.py
+++ dashboard/models.py
@@ -13,7 +13,7 @@ class Dealers(models.Model):
 
 class CreditNotes(models.Model):
     CN_ID = models.CharField(max_length=20, unique=True)
-    D_ID = models.CharField(max_length=10)
+    D_ID = models.ForeignKey(Dealers, on_delete=models.CASCADE, db_column='D_ID')
     TotalDocumentAmount = models.DecimalField(max_digits=38, decimal_places=20)
     TotalVATAmountDocumentt = models.DecimalField(max_digits=38, decimal_places=20)
     TotalDocumentAmountWithVAT = models.DecimalField(max_digits=38, decimal_places=20)
