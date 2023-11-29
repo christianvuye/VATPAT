0372bdc62858e4f318ddc6f0b67a32e039962bd1
christianvuye <christian.vuye.ext@honda-eu.com>
2023-11-29 11:28:15 +0100

    add ORM test for creditnote model database interaction

---
+++ dashboard/tests/integration/test_orm_setup_models_creditnotes.py
@@ -0,0 +1,24 @@
+import pytest
+from django.utils import timezone
+from dashboard.models import CreditNotes
+
+@pytest.fixture
+def create_credit_note():
+    now = timezone.now()  # timezone-aware datetime for the IssuedDate
+    return CreditNotes.objects.create(
+        CN_ID='CN001',
+        D_ID='D001',
+        TotalDocumentAmount=12345.67,
+        TotalVATAmountDocumentt=1234.56,
+        TotalDocumentAmountWithVAT=13580.23,
+        AccountingNumberID='AN12345',
+        IssuedDate=now
+    )
+
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
+def test_orm_setup_for_credit_notes(create_credit_note):
+    all_credit_notes = CreditNotes.objects.all()
+
+    # Ensure we can fetch credit notes, indicating no basic mismatch
+    assert all_credit_notes.exists(), "Failed to fetch credit notes from the database."
+    assert create_credit_note in all_credit_notes, "Created credit note is not in the database."
