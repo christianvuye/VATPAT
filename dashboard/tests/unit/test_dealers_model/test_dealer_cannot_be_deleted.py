855991922d1eb558c651b6c7d82583198239ca21
christianvuye <christian.vuye.ext@honda-eu.com>
2024-01-30 18:59:58 +0100

    Refactor test to use shared dealer instance fixture

--- dashboard/tests/unit/test_dealers_model/test_dealer_cannot_be_deleted.py
+++ dashboard/tests/unit/test_dealers_model/test_dealer_cannot_be_deleted.py
@@ -1,20 +1,5 @@
 import pytest
-#from django.utils import timezone
-#from dashboard.models import Dealers
 from django.db import IntegrityError, OperationalError, DatabaseError
-
-"""@pytest.fixture
-def dealer_instance():
-    # Create a Dealer instance
-    dealer = Dealers.objects.create(
-        D_ID="D001",
-        DealerName="Test Dealer",
-        DealerVATnumber="VAT12345678",
-        DealerEmail="dealer@example.com",
-        CreatedDate=timezone.now(),
-        ModifiedDate=timezone.now()
-    )
-    return dealer"""
 
 @pytest.mark.django_db
 def test_dealer_cannot_be_deleted(test_dealer_instance):
--- dashboard/tests/unit/test_dealers_model/test_dealers_model_duplicate_d_id_raises_error.py
+++ dashboard/tests/unit/test_dealers_model/test_dealers_model_duplicate_d_id_raises_error.py
@@ -3,21 +3,10 @@ from django.db.utils import IntegrityError
 from django.utils import timezone
 from dashboard.models import Dealers
 
-@pytest.fixture
-def create_initial_dealer():
-    return Dealers.objects.create(
-        D_ID="D001",
-        DealerName="First Dealer",
-        DealerVATnumber="VAT12345678",
-        DealerEmail="firstdealer@example.com",
-        CreatedDate=timezone.now(),
-        ModifiedDate=timezone.now()
-    )
-
 @pytest.mark.django_db
-def test_duplicate_d_id_raises_error(create_initial_dealer):
-    # Fixture 'create_initial_dealer' is used to create the first dealer instance
-    initial_dealer = create_initial_dealer
+def test_duplicate_d_id_raises_error(test_dealer_instance):
+    # Fixture 'test_dealer_instance' from conftest.py is used to create the first dealer instanc
+    initial_dealer = test_dealer_instance
 
     # Attempt to create another dealer with the same D_ID as the initial dealer
     with pytest.raises(IntegrityError) as excinfo:
