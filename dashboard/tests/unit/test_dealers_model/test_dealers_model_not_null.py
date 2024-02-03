3bee4487df3fc1edc4f97272cd88f114124a3819
christianvuye <christian.vuye.ext@honda-eu.com>
2024-02-03 13:31:25 +0100

    update test with simplicifcation

--- dashboard/tests/unit/test_dealers_model/test_dealers_model_not_null.py
+++ dashboard/tests/unit/test_dealers_model/test_dealers_model_not_null.py
@@ -4,7 +4,7 @@ from django.db.utils import IntegrityError
 from dashboard.models import Dealers
 
 @pytest.mark.django_db
-def test_dealers_model_not_null(dealer_data_all_null):
+def test_dealers_model_not_null(test_dealer_data_all_null):
     with transaction.atomic():    
         with pytest.raises(IntegrityError):
-            Dealers.objects.create(**dealer_data_all_null)
+            Dealers.objects.create(**test_dealer_data_all_null)
