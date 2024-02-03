5305d7f3bc9715a8f66c2efdee41217aa655e83b
christianvuye <christian.vuye.ext@honda-eu.com>
2024-02-03 10:01:22 +0100

    add test to check if timestamps get added upon creation of a new dealer instance

---
+++ dashboard/tests/unit/test_dealers_model/test_dealers_model_timestamps_existence.py
@@ -0,0 +1,12 @@
+import pytest
+
+@pytest.mark.django_db
+def test_dealers_timestamps_existence(test_dealer_instance_no_timestamps):
+    """
+    Test that `CreatedDate` and `ModifiedDate` fields in the Dealers model
+    are set upon instance creation.
+    """
+    dealer = test_dealer_instance_no_timestamps
+    
+    assert dealer.CreatedDate is not None, "CreatedDate not set on new instance"
+    assert dealer.ModifiedDate is not None, "ModifiedDate not set on new instance"
