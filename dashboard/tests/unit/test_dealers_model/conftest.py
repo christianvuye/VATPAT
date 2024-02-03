b14148c383cee1f578900d4b839b1e992a7b9ae4
christianvuye <christian.vuye.ext@honda-eu.com>
2024-02-03 12:12:26 +0100

    add test fixture where all dealer data is empty

--- dashboard/tests/unit/test_dealers_model/conftest.py
+++ dashboard/tests/unit/test_dealers_model/conftest.py
@@ -33,3 +33,14 @@ def test_dealer_data_no_timestamps():
 def test_dealer_instance_no_timestamps(test_dealer_data_no_timestamps):
     """Fixture to create a dealer instance without manually setting timestamps, using the provided dealer data."""
     return Dealers.objects.create(**test_dealer_data_no_timestamps)
+
+@pytest.fixture
+def test_dealer_data_all_null():
+    return {
+        "D_ID": None,  # Assuming this is a test case and D_ID is not an AutoField
+        "DealerName": None,
+        "DealerVATnumber": None,
+        "DealerEmail": None,
+        "CreatedDate": None,
+        "ModifiedDate": None,
+    }
