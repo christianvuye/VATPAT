9b01e20b60a2b03fb45772f13608d3b916a30742
christianvuye <christian.vuye.ext@honda-eu.com>
2024-02-14 14:37:15 +0100

    Rename test to check if dealer can be deleted

--- dashboard/tests/unit/test_dealers_model/test_dealers_model_delete.py
+++ dashboard/tests/unit/test_dealers_model/test_dealers_model_delete.py
@@ -2,7 +2,7 @@ import pytest
 from django.db import IntegrityError, OperationalError, DatabaseError
 
 @pytest.mark.django_db
-def test_dealers_models_delete(test_dealer_instance):
+def test_dealers_model_delete(test_dealer_instance):
     dealer = test_dealer_instance
 
     # Attempt to delete the dealer and expect an error
