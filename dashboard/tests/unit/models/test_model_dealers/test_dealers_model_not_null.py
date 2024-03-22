1e849e53f3f13b59bbc707b30d01d2d3fdf31c98
christianvuye <christian.vuye.ext@honda-eu.com>
2024-03-22 12:27:32 +0100

    Modify non-null validation check to ValidationError in Dealers model, instead of Integrity Error

--- dashboard/tests/unit/models/test_model_dealers/test_dealers_model_not_null.py
+++ dashboard/tests/unit/models/test_model_dealers/test_dealers_model_not_null.py
@@ -1,10 +1,9 @@
 import pytest
-from django.db import transaction
-from django.db.utils import IntegrityError
+from django.core.exceptions import ValidationError
 from dashboard.models import Dealers
 
 @pytest.mark.django_db
 def test_dealers_model_not_null(test_dealer_data_all_null):
-    with transaction.atomic():    
-        with pytest.raises(IntegrityError):
-            Dealers.objects.create(**test_dealer_data_all_null)
+    with pytest.raises(ValidationError):
+        dealer = Dealers(**test_dealer_data_all_null)
+        dealer.full_clean()  # This triggers the model validation
