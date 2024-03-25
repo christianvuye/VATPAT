846944aa0e286c64484a531d1aac654dfc4522c9
christianvuye <christian.vuye.ext@honda-eu.com>
2024-03-25 19:14:23 +0100

    Update DealerEmail to EmailField in Dealer model class

--- dashboard/models.py
+++ dashboard/models.py
@@ -6,7 +6,7 @@ class Dealers(models.Model):
     D_ID = models.CharField(max_length=10, unique=True)
     DealerName = models.CharField(max_length=100)
     DealerVATnumber = models.CharField(max_length=20)
-    DealerEmail = models.CharField(max_length=80)
+    DealerEmail = models.EmailField(max_length=80)
     CreatedDate = models.DateTimeField(auto_now_add=True)
     ModifiedDate = models.DateTimeField(auto_now=True)
 
