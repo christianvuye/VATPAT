9bc28f3741111e2000db6f7b4e83f62d9dd9a274
christianvuye <christian.vuye.ext@honda-eu.com>
2024-04-22 18:58:12 +0200

    update conftest test data for credit note model to include is active field for dealer reference and assign dealer ID at test

--- dashboard/tests/unit/models/test_model_creditnotes/conftest.py
+++ dashboard/tests/unit/models/test_model_creditnotes/conftest.py
@@ -12,6 +12,7 @@ def test_dealer_data():
         "DealerEmail": "dealer@example.com",
         "CreatedDate": timezone.now(),
         "ModifiedDate": timezone.now(),
+        "is_active": True
     }
 
 @pytest.fixture
@@ -20,8 +21,8 @@ def test_credit_note_data():
     Fixture to provide credit note data dictionary.
     """
     return {
-        "CN_ID": "CN20230001", #Check with Paulo if this is the correct format
-        "D_ID": "PT123456", #References the dealer above
+        "CN_ID": "CN20230001", 
+        #"D_ID": "PT123456", Omit the D_ID since it will be set to the dealer created during tests.
         "TotalDocumentAmount": Decimal('1234.56'),
         "TotalVATAmountDocumentt": Decimal('234.56'),
         "TotalDocumentAmountWithVAT": Decimal('1469.13'),
