c8818bbdf89023fbdf6b68a14fc28bf3bb834728
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-04 14:10:34 +0200

    add test to make sure the D_ID field cannot be empty when creating a credit note model instance

--- dashboard/tests/unit/models/test_model_creditnotes/test_creditnotes_model_D_ID_not_null.py
+++ dashboard/tests/unit/models/test_model_creditnotes/test_creditnotes_model_D_ID_not_null.py
@@ -1,19 +1,12 @@
 import pytest
 from django.core.exceptions import ValidationError
-from dashboard.models import CreditNotes, Dealers
+from dashboard.models import CreditNotes
 
 @pytest.mark.django_db
-def test_credit_notes_d_id_not_null(test_credit_note_data, test_dealer_data):
+def test_credit_notes_d_id_not_null(test_credit_note_data):
     """
     Test that the D_ID field in the CreditNotes model cannot be empty.
     """
-    # Creating a dealer to satisfy foreign key requirement
-    dealer = Dealers.objects.create(**test_dealer_data)
-    test_credit_note_data['D_ID'] = dealer
-
-    # Attempt to manually set D_ID to None or an invalid value to test model-level validation
-    test_credit_note_data['D_ID'] = None  # Simulate the missing D_ID scenario
-
     credit_note = CreditNotes(**test_credit_note_data)
 
     # Expecting a ValidationError when full_clean is called
@@ -22,4 +15,4 @@ def test_credit_notes_d_id_not_null(test_credit_note_data, test_dealer_data):
 
     # Check that the validation error is due to D_ID being required
     assert 'D_ID' in excinfo.value.error_dict
-    assert excinfo.value.error_dict['D_ID'][0].code == 'null'  # or 'blank', based on your validation setup
+    assert excinfo.value.error_dict['D_ID'][0].code == 'blank' 
