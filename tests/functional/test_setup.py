f2adb6a0880f7e52c80ba85a0fe8d48e1ba81ea8
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-16 01:33:12 +0200

    changed setup test to test for the name of dashboard title instead of congratulations as before

--- tests/functional/test_setup.py
+++ tests/functional/test_setup.py
@@ -3,5 +3,5 @@ from selenium import webdriver
 def test_initial_page_title():
     browser = webdriver.Firefox()
     browser.get("http://localhost:8000")
-    assert "Congratulations" in browser.title
+    assert "Dashboard" in browser.title
     browser.quit()  # Don't forget to close the browser to clean up
