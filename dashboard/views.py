8c8c337f04632091695809f157605f418b1d7f8c
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-18 05:24:31 +0200

    add a view to collect credit notes from previous month and return the template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .services import collect_credit_notes_from_previous_month
 
 def dashboard_view(request):
     return render(request, 'dashboard/dashboard.html')
+
+def credit_notes_previous_month_view(request):
+    credit_notes = collect_credit_notes_from_previous_month()
+    return render(request, 'dashboard/credit_notes_previous_month.html', {'credit_notes': credit_notes})
