8aba0841c336653eeaa5c970af7e47d8bf4ddfb4
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-18 06:03:29 +0200

    add url pattern for credit_notes_previous_month_grouped_by_dealer view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import dashboard_view, credit_notes_previous_month_view
+from .views import (
+    dashboard_view, 
+    credit_notes_previous_month_view,
+    credit_notes_previous_month_grouped_by_dealer_view
+)
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),
     path('credit_notes_previous_month/', credit_notes_previous_month_view, name='credit_notes_previous_month'),
+    path('credit_notes_previous_month_grouped_by_dealer/', credit_notes_previous_month_grouped_by_dealer_view, name='credit_notes_previous_month_grouped_by_dealer')
 ]
