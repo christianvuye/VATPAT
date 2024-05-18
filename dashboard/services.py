7576ef249760d11aa5d7976362ea41faf743fad8
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-18 05:52:21 +0200

    add function to collect the credit notes from the previous month grouped by dealer

--- dashboard/services.py
+++ dashboard/services.py
@@ -1,5 +1,6 @@
 from .models import CreditNotes
 from .utils import get_previous_month_date_range
+from collections import defaultdict
 
 def collect_credit_notes_from_previous_month():
     """
@@ -9,3 +10,15 @@ def collect_credit_notes_from_previous_month():
     credit_notes = CreditNotes.objects.filter(IssuedDate__range=[start_date, end_date])
     
     return credit_notes
+
+def collect_credit_notes_from_previous_month_grouped_by_dealer():
+    """
+    Collect all credit notes from the previous month and group them by dealer.
+    """
+    credit_notes = collect_credit_notes_from_previous_month()
+
+    grouped_credit_notes = defaultdict(list)
+    for note in credit_notes:
+        grouped_credit_notes[note.D_ID.DealerName].append(note)
+    
+    return grouped_credit_notes
