6c220331eebeee913d5579fc44bd175f4aebf611
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-18 05:34:01 +0200

    add url pattern to show the credit notes from the previous month template

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import dashboard_view
+from .views import dashboard_view, credit_notes_previous_month_view
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),
+    path('credit_notes_previous_month/', credit_notes_previous_month_view, name='credit_notes_previous_month'),
 ]
