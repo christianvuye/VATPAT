dd61220f4c8bdf30009cf4a370cd9a7763105132
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-18 05:55:34 +0200

    add view function to display credit notes from the previous month, grouped by dealer

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .services import collect_credit_notes_from_previous_month
+from .services import (
+    collect_credit_notes_from_previous_month,
+    collect_credit_notes_from_previous_month_grouped_by_dealer
+)
 
 def dashboard_view(request):
     return render(request, 'dashboard/dashboard.html')
@@ -7,3 +10,7 @@ def dashboard_view(request):
 def credit_notes_previous_month_view(request):
     credit_notes = collect_credit_notes_from_previous_month()
     return render(request, 'dashboard/credit_notes_previous_month.html', {'credit_notes': credit_notes})
+
+def credit_notes_previous_month_grouped_by_dealer_view(request):
+    credit_notes = collect_credit_notes_from_previous_month_grouped_by_dealer()
+    return render(request, 'dashboard/credit_notes_previous_month_grouped_by_dealer.html', {'credit_notes': credit_notes})
