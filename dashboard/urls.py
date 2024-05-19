c4f16d87ff381834fdba0ca518ddc58363c090d5
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-19 14:31:57 +0200

    update url file to make pattern code more readable

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,6 +7,10 @@ from .views import (
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),
-    path('credit_notes_previous_month/', credit_notes_previous_month_view, name='credit_notes_previous_month'),
-    path('credit_notes_previous_month_grouped_by_dealer/', credit_notes_previous_month_grouped_by_dealer_view, name='credit_notes_previous_month_grouped_by_dealer')
+    path('credit_notes_previous_month/', 
+         credit_notes_previous_month_view, 
+         name='credit_notes_previous_month'),
+    path('credit_notes_previous_month_grouped_by_dealer/', 
+         credit_notes_previous_month_grouped_by_dealer_view, 
+         name='credit_notes_previous_month_grouped_by_dealer')
 ]
