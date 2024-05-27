11ebb081374739f95a348c511a6a8a1abd01aaf0
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-27 16:15:39 +0200

    add model registration to admin interface

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Dealers, CreditNoteResumeEmail, CreditNotes, AcknowledgementRequest, AcknowledgementReceived
 
 # Register your models here.
+admin.site.register(Dealers)
+admin.site.register(CreditNoteResumeEmail)
+admin.site.register(CreditNotes)
+admin.site.register(AcknowledgementRequest)
+admin.site.register(AcknowledgementReceived)
