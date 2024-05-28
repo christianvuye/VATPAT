fe6352a93fb692699f6c43f9e828b5db9315a53e
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-28 17:06:15 +0200

    removed url route to template and view that showed credit notes resumes previous month grouped by deale

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import (
     dashboard_view, 
     credit_notes_previous_month_view,
-    credit_notes_previous_month_grouped_by_dealer_view,
     dashboard_2_view
 )
 
@@ -11,8 +10,5 @@ urlpatterns = [
     path('credit_notes_previous_month/', 
          credit_notes_previous_month_view, 
          name='credit_notes_previous_month'),
-    path('credit_notes_previous_month_grouped_by_dealer/', 
-         credit_notes_previous_month_grouped_by_dealer_view, 
-         name='credit_notes_previous_month_grouped_by_dealer'),
     path('dashboard_2/', dashboard_2_view, name='dashboard_2')
 ]
