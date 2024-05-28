7a9b04ea8fa8629a531541778d628bb90ff9afcf
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-28 17:03:41 +0200

    remove view function that rendered template that showed credit notes grouped by dealer from the previous month

--- dashboard/views.py
+++ dashboard/views.py
@@ -14,17 +14,6 @@ def credit_notes_previous_month_view(request):
     credit_notes = collect_credit_notes_from_previous_month()
     return render(request, 'dashboard/credit_notes_previous_month.html', {'credit_notes': credit_notes})
 
-def credit_notes_previous_month_grouped_by_dealer_view(request):
-    credit_notes = collect_credit_notes_from_previous_month()
-    unique_dealer_list = collect_unique_dealers_from_credit_notes(credit_notes)
-    credit_notes_grouped = credit_notes_previous_month_per_dealer_dict(credit_notes, 
-                                                                       unique_dealer_list
-                                                                       )
-    return render(request, 
-                  'dashboard/credit_notes_previous_month_grouped_by_dealer.html', 
-                  {'credit_notes': credit_notes_grouped}
-                  )
-
 def dashboard_2_view(request):
     credit_notes = collect_credit_notes_from_previous_month()
 
