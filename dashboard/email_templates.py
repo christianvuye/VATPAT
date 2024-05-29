41e341a552f712b78e48ac82bcb23beee2d0fb55
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-29 14:34:06 +0200

    add dictionary with email template content for credit note resume email

---
+++ dashboard/email_templates.py
@@ -0,0 +1,34 @@
+credit_note_email_template = {
+    "subject": 
+        "Protocolo ref. a regularização do I.V.A das Notas de Crédito.",
+    "body": 
+        """
+        <p>Para cumprimento do disposto no nº5 do Artigo 78º, do código do I.V.A.,<br>
+        agradecemos a resposta a este email, como tomaram conhecimento da regularização do
+        I.V.A. do(s) documento(s) em referência.</p>
+        """,
+    "table_header": 
+        """
+        <table border="1" cellpadding="5" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>Numero Doc</th>
+                    <th>Data</th>
+                    <th>I.V.A</th>
+                </tr>
+            </thead>
+            <tbody>
+        """,
+    "table_footer": 
+        """
+            </tbody>
+        </table>
+        """,
+    "signature": 
+        """
+        <p>Com os melhores cumprimentos,<br>
+                Honda Motor Europe Ltd<br>
+                Suc. Portugal<br>
+                Direcção Financeira</p>
+        """
+}
