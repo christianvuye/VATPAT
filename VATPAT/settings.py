8dcf398b8517111ac3ed6ce3daa250656b65937e
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-30 19:35:53 +0200

    add email settings

--- VATPAT/settings.py
+++ VATPAT/settings.py
@@ -127,3 +127,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "postmaster@sandbox14f2a85cb0f84709881e45e8852489c6.mailgun.org"
+EMAIL_HOST_PASSWORD ="ed206fb068ce7c8f491b4a26de88d286-0996409b-123b2635"
