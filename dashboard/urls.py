6a97a2df0bfaeca6d4773f1a00da35af5f27555a
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-31 12:41:23 +0200

    add url pattern for custom login view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
+    CustomLoginView,
     dashboard_view
 )
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('', dashboard_view, name='dashboard')
 ]
