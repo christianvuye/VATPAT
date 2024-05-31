3fd73879f39532a437b461074668538b92fc6760
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-31 22:24:54 +0200

    add url path to acknowledgements dashboard view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 from .views import (
     CustomLoginView,
-    dashboard_view
+    dashboard_view,
+    dashboard_view_acknowledgements
 )
 
 # clean up all the code related to required login before being able to access the dashboard
 # clean up all the different paths and urls too, because they are a mess.
 urlpatterns = [
     path('', CustomLoginView.as_view(), name='login'),
-    path('dashboard/', login_required(dashboard_view), name='dashboard')
+    path('dashboard/', login_required(dashboard_view), name='dashboard'),
+    path('dashboard_view_acknowledgements/', login_required(dashboard_view_acknowledgements), name='dashboard_view_acknowledgements')
 ]
