c0858208d5aca7843ba1bc2067c87a805f59a793
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-31 22:37:56 +0200

    add util function to get x amount of previous months and return a list of previous months in reverse

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -9,3 +9,13 @@ def get_previous_month_date_range():
     last_day_previous_month = first_day_current_month - timedelta(days=1)
     first_day_previous_month = last_day_previous_month.replace(day=1)
     return first_day_previous_month, last_day_previous_month
+
+def get_previous_months(num_months):
+    """
+    Returns a list of the previous months from the current month.
+    """
+    today = datetime.today()
+    previous_months = []
+    previous_months = [(today - timedelta(days=30 * i)).strftime("%b '%y") for i in range(num_months + 1)]
+    previous_months.reverse() # Reverse the list to show the oldest month first
+    return previous_months
