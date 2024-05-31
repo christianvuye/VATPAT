1f57d388dedaf7ca6967cd48c516a238c0c739ab
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-05-31 12:34:31 +0200

    add Custom Login View class that inherits from the LoginView class, for login page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 from .services import (
     collect_credit_notes_from_previous_month,
     collect_unique_dealers_from_credit_notes, 
@@ -6,6 +7,16 @@ from .services import (
     credit_notes_totals_per_dealer
 )
 
+class CustomLoginView(LoginView):
+    """
+    Custom login view that uses the login.html template.
+    """
+    template_name = 'login.html'
+
+# Django recommends using function-based views over class-based views when the view is simple.
+# In this case, the view is simple and does not require any additional methods or attributes.
+# Therefore, we use a function-based view. 
+# But check with Jessamyn what she thinks is the right approach.
 def dashboard_view(request):
     """
     Renders the dashboard view with the data from the previous month.
