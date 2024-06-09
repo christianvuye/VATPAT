1e47c357a94e513887e0b5ff42dbf6191f4b844d
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-06-09 22:35:54 +0200

    update dashboard view function to return the required data by query with Django's ORM

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Dealers
-from .utils import get_previous_months
+from .models import Dealers, CreditNotes
+from .utils import get_previous_months, get_previous_month_date_range
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count, Sum
 
 """
 Consider using class based views for everything.
@@ -23,8 +24,27 @@ def dashboard_view(request):
     """
     Renders the dashboard view with the data from the previous month.
     """
+    # Get the date range for the previous month
+    start_date, end_date = get_previous_month_date_range()
 
-    return render(request, 'dashboard/dashboard.html', 
+    # Get all credit notes issued in the previous month
+    credit_notes = CreditNotes.objects.filter(IssuedDate__range=[start_date, end_date])
+
+    # Get the unique dealers
+    dealers = credit_notes.values('D_ID').distinct()
+
+    # Group and aggregate the credit notes by dealer
+    credit_notes_aggregated = credit_notes.values('D_ID').annotate(
+        total_credit_notes=Count('CN_ID'),
+        total_document_amount=Sum('TotalDocumentAmount'),
+        total_vat_amount=Sum('TotalVATAmountDocumentt'),
+        total_document_amount_with_vat=Sum('TotalDocumentAmountWithVAT')
+    )
+
+    return render(request, 'dashboard/dashboard.html', {
+        'dealers': dealers,
+        'credit_notes': credit_notes_aggregated
+    }
                   )
 
 @login_required
