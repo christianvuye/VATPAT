1ed647ce893f534f8051d2dab26b67575140ff72
Christian Vuye EXT <christian.vuye.ext@honda-eu.com>
2024-07-02 18:01:09 +0200

    remove obsolete pytest functional and integration tests that were used for test driven develomment

--- tests/functional/test_setup.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-def test_initial_page_title():
-    browser = webdriver.Firefox()
-    browser.get("http://localhost:8000")
-    assert "Dashboard" in browser.title
-    browser.quit()  # Don't forget to close the browser to clean up
--- tests/unit/test_secret_key.py
+++
@@ -1,5 +0,0 @@
-from django.conf import settings
-
-def test_secret_key():
-    assert hasattr(settings, 'SECRET_KEY'), "SECRET_KEY setting not found"
-    assert settings.SECRET_KEY != '', "SECRET_KEY is empty"
